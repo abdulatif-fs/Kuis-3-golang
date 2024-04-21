af77e9302c10055651df409ad87ba758f4f98b1d
abdulatif-fs <fasijardiq@gmail.com>
2024-04-21 20:26:25 +0700

    Update Struct

--- Quiz-3/structs/structs.go
+++ Quiz-3/structs/structs.go
@@ -1,26 +1,24 @@
 package structs
 
-import (
-	"github.com/augurysys/timestamp"
-)
+// "github.com/augurysys/timestamp"
 
 type Categories struct {
-	ID         int                 `json:"id"`
-	Nama       string              `json:"nama"`
-	Created_at timestamp.Timestamp `json:"created_at"`
-	Updated_at timestamp.Timestamp `json:"updated_at"`
+	ID         int         `json:"id"`
+	Nama       string      `json:"nama"`
+	Created_at interface{} `json:"created_at"`
+	Updated_at interface{} `json:"updated_at"`
 }
 
 type Book struct {
-	ID           int                 `json:"id"`
-	Title        string              `json:"title"`
-	Description  string              `json:"description"`
-	Image_url    string              `json:"image_url"`
-	Release_year int                 `json:"release_year"`
-	Price        string              `json:"price"`
-	Total_page   int                 `json:"total_price"`
-	Thickness    string              `json:"thickness"`
-	Created_at   timestamp.Timestamp `json:"created_at"`
-	Updated_at   timestamp.Timestamp `json:"updated_at"`
-	Category_id  int                 `json:"category_id"`
+	ID           int         `json:"id"`
+	Title        string      `json:"title"`
+	Description  string      `json:"description"`
+	Image_url    string      `json:"image_url"`
+	Release_year int         `json:"release_year"`
+	Price        string      `json:"price"`
+	Total_page   int         `json:"total_price"`
+	Thickness    string      `json:"thickness"`
+	Created_at   interface{} `json:"created_at"`
+	Updated_at   interface{} `json:"updated_at"`
+	Category_id  int         `json:"category_id"`
 }
